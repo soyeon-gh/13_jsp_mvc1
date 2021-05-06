40870142b8061c52cef53cc1d90d3352c2341641
19시_웹개발_ksy <19__ksy@DESKTOP-F0Q81EV>
2021-05-06 21:27:44 +0900

    commit

--- 13_jsp_mvc1/src/step3_00_boardEx/Dao_test_01.java
+++ 13_jsp_mvc1/src/step3_00_boardEx/Dao_test_01.java
@@ -81,7 +81,7 @@ public class Dao_test_01 {
 	 * pstmt에 conn.pre(변수)
 	 * 
 	 * pstmt에 세팅 ( 밸류값 ? 만 : string만 )
-	 * 
+	 * pstmt.exe
 	 * }
 	 * 
 	 * catch { }
@@ -100,8 +100,12 @@ public class Dao_test_01 {
 			
 			pstmt = conn.prepareStatement(sql);
 			
-			pstmt.setString(1, g);
-			
+			pstmt.setString(1, dto_test_1.getWriter());
+			pstmt.setString(2, dto_test_1.getEmail());
+			pstmt.setString(3, dto_test_1.getSubject());
+			pstmt.setString(4, dto_test_1.getPassword());
+			pstmt.setString(5, dto_test_1.getContent());
+			pstmt.executeUpdate();
 			
 			
 		} catch (Exception e) {
@@ -152,43 +156,67 @@ public class Dao_test_01 {
 	 *  finally { close }
 	 *  
 	 */
-	public ArrayList<Dto_test_1> getAllBoard() {
+	
+	
+	public ArrayList<Dto_test_1> getAllBoard(){
 		
-		ArrayList<Dto_test_1> boardList = new ArrayList<Dto_test_1>(); // ArrayList생성
-		Dto_test_1 bdto = null;	// DTO 변수생성해서 초기화
-		conn = getConnection(); // conn 에 연결정보 가져오기
+		ArrayList<Dto_test_1> boardList = new ArrayList<Dto_test_1>();
+		Dto_test_1 bdto = null;
+		conn = getConnection();
 		
 		try {
-			pstmt = conn.prepareStatement("SELECT * FROM BOARD");
+			
+			
+			pstmt = conn.prepareStatement("SELETE * FROM BOARD");
 			rs = pstmt.executeQuery();
 			
 			while (rs.next()) {
-				
 				bdto = new Dto_test_1();
 				bdto.setNum(rs.getInt(1));
 				bdto.setWriter(rs.getString(2));
 				bdto.setEmail(rs.getString(3));
 				bdto.setSubject(rs.getString(4));
 				bdto.setPassword(rs.getString(5));
-				bdto.setRegDate(sdf.format(rs.getString(6)));
+				bdto.setRegDate(sdf.format(rs.getDate(6)));
 				bdto.setReadCount(rs.getInt(7));
 				bdto.setContent(rs.getString(8));
-				
 				boardList.add(bdto);
-				
 			}
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if (rs!=null) {try {rs.close();} catch (SQLException e) {e.printStackTrace();}}
-			if (pstmt!=null) {try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}}
-			if (conn!=null) {try {conn.close();} catch (SQLException e) {e.printStackTrace();}}
+			if (rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if (pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if (conn!=null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		
-		
 		return boardList;
 		
+		
+		
 	}
+	
 	
 	// 게시글 한 개 조회하기
 	/*public Dto_test_1 getOneBoard(int num)
@@ -223,8 +251,8 @@ public class Dao_test_01 {
 		Dto_test_1 bdto = new Dto_test_1();
 		
 		try {
-			conn = getConnection();
 			
+			conn = getConnection();
 			pstmt = conn.prepareStatement("UPDATE BOARD SET READ_COUNT = READ_COUNT + 1 WHERE NUM=?");
 			pstmt.setInt(1, num);
 			pstmt.executeUpdate();
@@ -233,7 +261,8 @@ public class Dao_test_01 {
 			pstmt.setInt(1, num);
 			rs = pstmt.executeQuery();
 			
-			if(rs.next()) {
+			if (rs.next()) {
+				
 				bdto.setNum(rs.getInt("num"));
 				bdto.setWriter(rs.getString("writer"));
 				bdto.setEmail(rs.getString("email"));
@@ -242,8 +271,8 @@ public class Dao_test_01 {
 				bdto.setRegDate(sdf.format(rs.getDate("reg_date")));
 				bdto.setReadCount(rs.getInt("read_count"));
 				bdto.setContent(rs.getString("content"));
+				
 			}
-			
 			
 			
 		} catch (Exception e) {
@@ -253,37 +282,32 @@ public class Dao_test_01 {
 				try {
 					rs.close();
 				} catch (SQLException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
 			}
+			
 			if (pstmt!=null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
 			}
 			if (conn!=null) {
 				try {
 					conn.close();
 				} catch (SQLException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
 			}
 		}
 		
-		
 		return bdto;
 		
-		
 	}
-	
-	
-	
-	
 	
 	
 	// 수정할 게시글 한 개 조회
@@ -308,8 +332,7 @@ public class Dao_test_01 {
 	 * return bdto
 	 */
 	
-	
-	public Dto_test_1 getOneupdateBoard(int num) {
+	public Dto_test_1 getOneUpdateBoard(int num) {
 		
 		Dto_test_1 bdto = new Dto_test_1();
 		
@@ -321,7 +344,7 @@ public class Dao_test_01 {
 			pstmt.setInt(1, num);
 			rs = pstmt.executeQuery();
 			
-			if(rs.next()) {
+			if (rs.next()) {
 				bdto.setNum(rs.getInt("num"));
 				bdto.setWriter(rs.getString("writer"));
 				bdto.setEmail(rs.getString("email"));
@@ -330,9 +353,7 @@ public class Dao_test_01 {
 				bdto.setRegDate(sdf.format(rs.getDate("reg_date")));
 				bdto.setReadCount(rs.getInt("read_count"));
 				bdto.setContent(rs.getString("content"));
-				
 			}
-			
 			
 			
 		} catch (Exception e) {
@@ -345,7 +366,6 @@ public class Dao_test_01 {
 					e.printStackTrace();
 				}
 			}
-			
 			if (pstmt!=null) {
 				try {
 					pstmt.close();
@@ -357,16 +377,15 @@ public class Dao_test_01 {
 				try {
 					conn.close();
 				} catch (SQLException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
 		
-		
-		
 		return bdto;
+		
 	}
+	
 	
 	
 	// 게시글 수정
@@ -391,60 +410,7 @@ public class Dao_test_01 {
 	 * 불린변수 반환
 	 * 
 	 */
-	public boolean updateBoard(Dto_test_1 dto_test_1) {
-		
-		boolean isUpdate = false;
-		
-		
-		try {
-			
-			conn = getConnection();
-			
-			pstmt = conn.prepareStatement("SELECT * FROM BOARD WHERE NUM=? AND PASSWORD=?");
-			pstmt.setInt(1, dto_test_1.getNum());
-			pstmt.setString(2, dto_test_1.getPassword());
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
-				pstmt = conn.prepareStatement("UPDATE BOARD SET SUBJECT=?, CONTENT=? WHERE NUM=?");
-				pstmt.setString(1, dto_test_1.getSubject());
-				pstmt.setString(2, dto_test_1.getContent());
-				pstmt.setInt(3, dto_test_1.getNum());
-				pstmt.executeUpdate();
-				isUpdate = true;
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (rs!=null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			if (pstmt!=null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			if (conn!=null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		return isUpdate;
-		
-	}
+	
 	
 	// 게시글 삭제
 	/*
